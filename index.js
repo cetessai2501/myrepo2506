7a2fa769a4607a44626a6b0b35328921458a2cc6
cetessai2501 <53481798+cetessai2501@users.noreply.github.com>
2019-08-07 13:05:55 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const http = require('http');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.use(express.static(path.join(__dirname, 'client');
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/index.html'));
+});
+
+const port = process.env.PORT || '4000';
+app.set('port', port);
+server.listen(port, () => console.log(`Running on : ${port}`);
